01af372895cccff1be6c225d55e21ed993463512
Johannes <johannes@soundofsynergy.com>
2024-04-14 13:01:18 +0200

    FightMusic

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,8 +7,8 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
     
-    public Sound[] musicSounds, sfxSounds;
-    public AudioSource musicSource, sfxSource;
+    public Sound[] musicSounds, fairyMusicSounds, foodMusicSounds, undergroundMusicSounds, sfxSounds;
+    public AudioSource musicSource, fairyMusicSource, foodMusicSource, undergroundMusicSource, sfxSource;
 
     private void Awake()
     {
@@ -22,23 +22,7 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
-    public void PlayMusic(string name)
-    {
-        Sound s = Array.Find(musicSounds, x => x.name == name);
 
-        if (s == null)
-        {
-            Debug.Log("Music Not Found");
-        }
-
-        else
-        {
-            musicSource.clip = s.clip;
-            musicSource.Play();
-        }
-    }
-    
     public void PlaySFX(string name)
     {
         Sound s = Array.Find(sfxSounds, x => x.name == name);
@@ -53,7 +37,84 @@ public class AudioManager : MonoBehaviour
             sfxSource.PlayOneShot(s.clip);
         }
     }
+    public void PlayFightMusic()
+    {
+        if (GameManager.Instance.GetAmountOfItemsOfPlayer(BodyPartSO.Type.FairyTale) >= 1)
+        {
+            PlayFairyMusic("FairyMusicLow");
+        }
+        else if (GameManager.Instance.GetAmountOfItemsOfPlayer(BodyPartSO.Type.FairyTale) >= 3)
+        {
+            PlayFairyMusic("FairyMusicHigh");
+        }
+          
+        if (GameManager.Instance.GetAmountOfItemsOfPlayer(BodyPartSO.Type.Food) >= 1)
+        {
+            PlayFairyMusic("FoodMusicLow");
+        }
+        else if (GameManager.Instance.GetAmountOfItemsOfPlayer(BodyPartSO.Type.Food) >= 3)
+        {
+            PlayFairyMusic("FoodMusicHigh");
+        }
+        
+        if (GameManager.Instance.GetAmountOfItemsOfPlayer(BodyPartSO.Type.Food) >= 1)
+        {
+            PlayFairyMusic("UndergroundMusicLow");
+        }
+        else if (GameManager.Instance.GetAmountOfItemsOfPlayer(BodyPartSO.Type.Food) >= 3)
+        {
+            PlayUndergroundMusic("UndergroundMusicHigh");
+        }
+    }
+    
+    public void PlayFairyMusic(string name)
+    {
+        Sound s = Array.Find(fairyMusicSounds, x => x.name == name);
 
+        if (s == null)
+        {
+            Debug.Log("Music Not Found");
+        }
+
+        else
+        {
+            fairyMusicSource.clip = s.clip;
+            fairyMusicSource.Play();
+        }
+    }
+    public void PlayFoodMusic(string name)
+    {
+        Sound s = Array.Find(foodMusicSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Music Not Found");
+        }
+
+        else
+        {
+            foodMusicSource.clip = s.clip;
+            foodMusicSource.Play();
+        }
+    }    
+    public void PlayUndergroundMusic(string name)
+    {
+        Sound s = Array.Find(undergroundMusicSounds, x => x.name == name);
+        
+        if (s == null)
+        {
+            Debug.Log("Music Not Found");
+        }
+
+        else
+        {
+            undergroundMusicSource.clip = s.clip;
+            undergroundMusicSource.Play();
+        }
+    }    
+
+    
+    
     /*public void StopSFX()
     {
         sfxSource.DOFade(0, .3f).OnComplete(() =>
